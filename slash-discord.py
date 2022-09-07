dca8d4bd50bfc9bee4c6b1c515a35f4953d6b8c4
whitewaffles <113029001+whitewaffles@users.noreply.github.com>
2022-09-07 22:28:15 +0900

    Update slash-discord.py

--- slash-discord.py
+++ slash-discord.py
@@ -17,6 +17,10 @@ import asyncio, discord
 import discord, datetime
 import interactions
 
+import os
+
+
+
 class aclient(discord.Client):
     def __init__(self):
         super().__init__(intents = discord.Intents.default())
@@ -118,6 +122,6 @@ async def slash2(interaction: discord.Interaction, 아이디: str, 답변: str):
 
 
 
+access_token = os.environ["BOT_TOKEN"]
 
-
-client.run('OTcwNjg0NjU5MjQ5ODYwNzE4.GbDwoP.pJ5Dvy54sR1lWA8FbbNra8u16NeUqy7WBNqbRI')
+client.run(access_token)
